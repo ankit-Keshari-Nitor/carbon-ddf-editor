c996a0256325c00d0cafe657437d6d4098522b67
Ankit Keshari <ankit.keshari@nitorinfotech.com>
2024-03-06 11:36:38 +0530

    CSS level changes and removes the tab from editor

--- src/components/editor/editor.js
+++ src/components/editor/editor.js
@@ -37,33 +37,22 @@ export default function Editor() {
   }
 
   return (
-    <Tabs>
-      <TabList aria-label="List of tabs" activation="manual">
-        <Tab>Edit</Tab>
-        <Tab>PreView</Tab>
-      </TabList>
-      <TabPanels>
-        <TabPanel>
-          <Grid className="editor-container">
-            <Column lg={4} className="palette-container">
-              <LeftPalette />
-            </Column>
-            <Column lg={8}>
-              <div
-                ref={drop}
-                style={{ height: "100%", backgroundColor }}
-                className="canvas-container"
-              >
-                <Canvas schema={schema} />
-              </div>
-            </Column>
-            <Column lg={4} className="right-container">
-              <RightPalette />
-            </Column>
-          </Grid>
-        </TabPanel>
-        <TabPanel>PreView</TabPanel>
-      </TabPanels>
-    </Tabs>
+    <Grid className="editor-container">
+      <Column lg={4} className="palette-container">
+        <LeftPalette />
+      </Column>
+      <Column lg={8}>
+        <div
+          ref={drop}
+          style={{backgroundColor }}
+          className="canvas-container"
+        >
+          <Canvas schema={schema} />
+        </div>
+      </Column>
+      <Column lg={4} className="right-container">
+        <RightPalette />
+      </Column>
+    </Grid>
   );
 }
