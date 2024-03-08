e46801e8f1da4c07eeeff12f4ce27895fb55eeec
Ankit Kesari <Ankit.Kesari@precisely.com>
2024-03-08 16:33:40 +0530

    Worked on the isRequired Field of select input field

--- src/components/form-fields/basic/select/select.js
+++ src/components/form-fields/basic/select/select.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { Select as CarbonSelect, SelectItem } from "@carbon/react";
 import { FORM_FIELD_TYPE, editableProps } from "../../../constant";
+import Label from "../label/label";
 
 const type = FORM_FIELD_TYPE.SELECT;
 
-const Select = (props) => {
-  const { field, ...rest } = props;
+const Select = ({ field }) => {
+  const { id, type, labelText, isRequired, ...rest } = field;
 
   return (
-    <CarbonSelect id={field.id} labelText={field.labelText} {...rest}>
-      <SelectItem text={field.text} value={field.value} />
-    </CarbonSelect>
+    <>
+      <Label labelText={labelText} isRequired={isRequired} />
+      <CarbonSelect id={id} labelText="" type={type} {...rest}>
+        <SelectItem text="No Option" value="no-option" />
+      </CarbonSelect>
+    </>
   );
 };
 
